962d1dc5cbe4b1b8c487324ebe4cc5a84e1ae135
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2024-03-02 16:58:31 +0530

    Variable type

--- Javascript/variable.js
+++ Javascript/variable.js
@@ -39,6 +39,71 @@ console.table({
 // const SCHOOL_NAME - recommended
 
 //  for classes
-//   SchoolName - upper camel case(Pascel Naming convention) -> full recommended
+//   SchoolName - upper camel case(Pascal Naming convention) -> full recommended
 
 // variable(data) type
+// number
+var numberVal = 2;
+var totalScore = 900;
+console.log(numberVal, typeof numberVal);
+// string
+const nameVal = "Dinesh";
+console.log(nameVal, typeof nameVal);
+const nameVal1 = "Gopal";
+console.log(nameVal1, typeof nameVal1);
+const nameVal2 = `Chand`;
+console.log(nameVal2, typeof nameVal2);
+// const nameVal3 = new String("Chand"); // never use it
+// console.log(nameVal3, typeof nameVal3);
+
+// Boolean
+const isActive = true;
+console.log(isActive, typeof isActive);
+
+// undefined
+var someVariable;
+console.log(someVariable, typeof someVariable);
+
+// null
+var nullValue = null;
+console.log(nullValue, typeof nullValue);
+
+// Array
+// var nameList = new Array("Dinesh", "Gopal", "Chand", 1); // not required
+
+// console.log(nameList, typeof nameList);
+var nameList1 = ["Dinesh", "Gopal", "Chand", 1];
+
+console.log({
+  value: nameList1,
+  typeof: typeof nameList1,
+  isArray: Array.isArray(nameList1),
+  length: nameList1.length,
+  firstVal: nameList1[0],
+});
+
+var nameAddressList = [
+  ["Dinesh", "Gopal", "Chand"],
+  ["st1", "st2", "blr", 560036],
+];
+
+console.log(
+  nameAddressList,
+  typeof nameAddressList,
+  Array.isArray(nameAddressList)
+);
+
+// Object type
+var nameAddressObject = {
+  name: { fname: "Dinesh", mname: "Gopal", lname: "Chand" },
+  address: { street1: "st1", street2: "st2", city: "blr", pincode: 560036 },
+};
+
+console.log(nameAddressObject.name.lname);
+
+console.log({
+  value: nameAddressObject,
+  typeof: typeof nameAddressObject,
+  isArray: Array.isArray(nameAddressObject),
+  firstVal: nameAddressObject.name.fname,
+});
