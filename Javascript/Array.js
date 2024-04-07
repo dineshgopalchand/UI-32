7a575991f4e09b3dda5dd1a9d6c6160c690452a5
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2024-04-07 14:10:25 +0530

    Array concat and sorting discussed

---
+++ Javascript/Array.js
@@ -0,0 +1,65 @@
+// var nameList1 = ["Dinesh", "Gopal", "Chand"];
+// var nameList2 = ["Riyaz", "Ahmad"];
+// var nameAddressList = [nameList1, ["st1", "st2", "blr", 560036]];
+
+// // var newNameList = [];
+// // for (const namVal of nameList1) {
+// //   newNameList[newNameList.length] = namVal;
+// // }
+// // for (const namVal of nameList2) {
+// //   newNameList[newNameList.length] = namVal;
+// // }
+
+// // console.log(newNameList);
+
+// const mergedNameList = [...nameList1, ...nameList2];
+// console.log(mergedNameList);
+// // console.log([nameList1, nameList2]);
+
+// var newNameList = nameList1;
+// var newNameList1 = [...nameList1]; // true copy(single dimension)
+// console.log({ newNameList, nameList1, newNameList1 });
+
+// nameList1.push("xyz");
+// console.log({ newNameList, nameList1, newNameList1 });
+// newNameList.push("xyzzzzz");
+// console.log({ newNameList, nameList1, newNameList1 });
+
+// const nameAddressListCopy = [...nameAddressList]; // deep copy only first level(multi dimension)
+// // const nameAddressListCopy = [
+// //     ...[[...nameAddressList[0]], [...nameAddressList[1]]],
+// //   ];
+
+// var nameList1 = ["Dinesh", "Gopal", "Chand"];
+// var nameList2 = ["Riyaz", "Ahmad"];
+// var nameList3 = ["Afreen"];
+// var nameAddressList = [nameList1, ["st1", "st2", "blr", 560036]];
+
+// // const concatenatedValue = nameList1.concat(nameList2, nameList3);
+// const concatenatedValue = [].concat(nameList1, nameList2, nameList3, nameList2);
+// console.log({ concatenatedValue });
+
+// // concatenatedValue.shift();
+// // console.log({ concatenatedValue });
+
+// // shift(from beginning)/pop(from end) - to delete node
+// // unshift(from beginning)/push(from end) - to add new node
+
+// // concatenatedValue.splice(0, 0, "zaa");
+// // concatenatedValue.splice(0, 3, "zaa");
+// concatenatedValue.splice(5, 3, "zaa");
+// console.log({ concatenatedValue });
+
+// sort
+
+var nameList1 = ["Dinesh", "Gopal", "Chand", "chand", "a", "ab"];
+// nameList1.sort();
+// nameList1.sort(function (n1, n2) {
+//   if (n1.length > n2.length) {
+//     return 1;
+//   } else {
+//     return -1;
+//   }
+// });
+nameList1.sort((n1, n2) => (n1.length > n2.length ? 1 : -1));
+console.log(nameList1);
